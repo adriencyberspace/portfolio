fb71654010e0a3ee688d65315b36415dab59ee15
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-02-16 17:46:20 -0600

    Edit test code spacing and simplify API URL var

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 // Script to test my API
 
-const api_projects = "https://adrienyoungcom-strapi-backend.herokuapp.com/projects"
 const api_url = "https://adrienyoungcom-strapi-backend.herokuapp.com"
 
 function fetchData() {
-  fetch(api_projects).then(response => {
+  fetch(api_url + `/projects`).then(response => {
     if (!response.ok) {
       throw Error('ERROR');
     }
     return response.json()
   }).then(data => {
     const html = data.map(project => {
+
       return `<p>${project.title}</p>
       <p>${api_url + project.primaryphoto.url}</p>
       <img src="${api_url + project.primaryphoto.formats.medium.url}">`
+      
     }).join('')
     document
       .querySelector('#app')
