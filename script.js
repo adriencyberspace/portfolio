a1dd6f619c17d8ac00790332ac7ff029f9fc69ed
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-02-17 14:05:36 -0600

    Move test.js to script.js and delete test.js

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ document.addEventListener('DOMContentLoaded', function() {
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
+const api_url = "https://adrienyoungcom-strapi-backend.herokuapp.com/projects"
+
+console.log("Works");
+
+function fetchData() {
+  fetch(api_url).then(response => {
+    if (!response.ok) {
+      throw Error('ERROR');
+    }
+    return response.json()
+  }).then(projects => {
+    for (let project in projects) {
+      
+      const current = projects[project];
+      console.log(current.title);
+
+      const project_navlink = document.createElement("a");
+      project_navlink.setAttribute("id", current.id);
+      project_navlink.setAttribute("href", '#');
+      project_navlink.innerHTML =`${current.title}`;
+
+      document.querySelector("#projlinks").appendChild(project_navlink);
+    
+    
+    }
+  }).catch(error => {
+    console.log(error)
+  });
+}
+
+fetchData();
+
