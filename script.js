1cc3748e9b286c2beb5043e42ed50c8b75388b04
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-02-19 18:19:18 -0600

    Passed API project details to #infodiv

--- script.js
+++ script.js
@@ -20,7 +20,11 @@ function fetchData() {
     const projects = data.map(project => ({
       id: project.id,
       title: project.title,
-      image: project.primaryphoto.url
+      description: project.description,
+      link: project.link,
+      image: project.primaryphoto.formats.large.url,
+      skills: project.skills
+
     }));
 
     for (let project in projects) {
@@ -31,17 +35,19 @@ function fetchData() {
       const project_navlink = document.createElement("a");
       project_navlink.setAttribute("id", "project" + current.id);
       project_navlink.setAttribute("href", '#');
-      project_navlink.innerHTML =`${current.title}`;
+      project_navlink.innerHTML =`<span>${current.title}</span>`;
       document.querySelector("#projlinks").appendChild(project_navlink);
 
       // Display project image upon clicking link
       project_navlink.onclick = () => {
-        displayImage()
+        displayImage();
+        displayInfo();
       };
 
       // Display project image upon hovering over link
       project_navlink.onmouseover = () => {
-        displayImage()
+        displayImage();
+        displayInfo();
       };
 
       // Display project image
@@ -52,6 +58,31 @@ function fetchData() {
         project_image.setAttribute("src", current.image);
         project_image.setAttribute("class", "primaryphoto");
         image_div.appendChild(project_image);
+        console.log()
+      };
+
+      // Display project info
+      function displayInfo() {
+        info_div = document.querySelector("#infodiv");
+        info_div.innerHTML = "";
+        let project_info = document.createElement("div");
+        project_info.innerHTML = `
+        <h3>${current.title}</h3>
+        <p>${current.description}</p>
+        <a href="${current.link}">Link to Project</a>
+        `
+        info_div.appendChild(project_info);
+        
+        for (let skill in current.skills) {
+          const this_skill = current.skills[skill];
+          console.log(this_skill.name);
+
+          skill_list = document.querySelector("#skillslist");
+          let project_skills = document.createElement("li");
+          project_skills.innerHTML = this_skill.name;
+          info_div.appendChild(project_skills);
+
+        }
       }
     }
 
