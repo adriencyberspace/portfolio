84e387ef9066b0b2b0d198af33fb4bf2fae7ad11
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-03-10 10:51:43 -0800

    Added WORK and arrows functionality on mobile

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ function fetchData() {
 
     }));
 
+
+
     for (let project in projects) {
       
       const current = projects[project];
@@ -41,45 +43,79 @@ function fetchData() {
 
       // Display project image upon clicking link
       project_navlink.onclick = () => {
-        displayImage();
-        displayInfo();
+        displayImage(current);
+        displayInfo(current);
       };
 
       // Display project image upon hovering over link
       project_navlink.onmouseover = () => {
-        displayImage();
-        displayInfo();
+        displayImage(current);
+        displayInfo(current);
       };
-  
+
+      document.querySelector('#adrien').onclick = () => {
+        document.querySelector('#arrowbar').style.display = 'none';
+      }
+
+      // Mobile view - show first project upon clicking WORK, then navigate forward and backward with arrows
+      document.querySelector('#work').onclick = () => {
+        document.querySelector('#arrowbar').style.display = 'flex';
+        let y = 0;
+        displayImage(projects[y]);
+        displayInfo(projects[y]);
+        
+        document.querySelector('#rightarrow').onclick = () => {
+          y += 1;
+          if (y >= projects.length) {
+            y = 0;
+          } 
+          displayImage(projects[y]);
+          displayInfo(projects[y]);
+        }
+
+        document.querySelector('#leftarrow').onclick = () => {
+          y -= 1;
+          if (y < 0) {
+            y = projects.length - 1;
+          } 
+          displayImage(projects[y]);
+          displayInfo(projects[y]);
+        }
+        
+      }
+
+
+
+
       // Display project image
-      function displayImage() {
+      function displayImage(x) {
         image_div = document.querySelector("#imagediv");
         image_div.innerHTML = "";
         let project_image = document.createElement("img");
-        project_image.setAttribute("src", current.image);
+        project_image.setAttribute("src", x.image);
         project_image.setAttribute("class", "primaryphoto");
         image_div.appendChild(project_image);
         console.log()
       };
 
       // Display project info
-      function displayInfo() {
+      function displayInfo(x) {
         title_div = document.querySelector("#titlediv");
         title_div.innerHTML = "";
         let project_title = document.createElement("h1");
-        project_title.innerHTML = `${current.title}`;
+        project_title.innerHTML = `${x.title}`;
         title_div.appendChild(project_title);
 
         // Display project description (hacky but works)
         // Need narrow and wide to keep rotated divs in place
         let project_desc_wide = document.createElement("p");
-        project_desc_wide.innerHTML = `${current.description}`;
+        project_desc_wide.innerHTML = `${x.description}`;
         desc_div_wide = document.querySelector("#descriptiondivwide");
         desc_div_wide.innerHTML = "";
         desc_div_wide.appendChild(project_desc_wide);
 
         let project_desc_narrow = document.createElement("p");
-        project_desc_narrow.innerHTML = `${current.description}`;
+        project_desc_narrow.innerHTML = `${x.description}`;
         desc_div_narrow = document.querySelector("#descriptiondivnarrow");
         desc_div_narrow.innerHTML = "";
         desc_div_narrow.appendChild(project_desc_narrow);
@@ -87,18 +123,18 @@ function fetchData() {
         
         // Display project links
         live_link = document.querySelector("#live-link");
-        live_link.href = `${current.link}`;
+        live_link.href = `${x.link}`;
         github_link = document.querySelector("#github-link");
-        github_link.href = `${current.githublink}`;
-        if (current.githublink == null) {
+        github_link.href = `${x.githublink}`;
+        if (x.githublink == null) {
           github_link.style.display = "none";
           live_link.style.display = "block";
           live_link.style.margin = "auto";
-        } else if (current.link == null) {
+        } else if (x.link == null) {
           live_link.style.display = "none";
           github_link.style.display = "block";
           github_link.style.margin = "auto";
-        } else if (current.githublink == null && current.link == null) {
+        } else if (x.githublink == null && x.link == null) {
           github_link.style.display = "none";
           live_link.style.display = "none";
         } else {
