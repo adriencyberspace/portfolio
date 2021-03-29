f99aa4f69f5caeaf30e2c1ac42b0137138c0bd3b
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-03-29 13:03:27 -0700

    Add vertical scroll for rotated projbar div

--- script.js
+++ script.js
@@ -199,5 +199,23 @@ function fetchData() {
   });
 }
 
+// Up and Down Scroll Function for Rotated #Projbar Div.
+(function() {
+  function scrollHorizontally(e) {
+      e = window.event || e;
+      var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+      document.getElementById('projbar').scrollLeft -= (delta*40); // Multiplied by 40
+      e.preventDefault();
+  }
+  if (document.getElementById('projbar').addEventListener) {
+      // IE9, Chrome, Safari, Opera
+      document.getElementById('projbar').addEventListener("mousewheel", scrollHorizontally, false);
+      // Firefox
+      document.getElementById('projbar').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+  } else {
+      // IE 6/7/8
+      document.getElementById('projbar').attachEvent("onmousewheel", scrollHorizontally);
+  }
+})();
 
 
